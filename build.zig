a35174384422ce1459919e6410e5159b7c4f4992
Frostre_ <n9043395@gmail.com>
2026-08-29 09:51:59 +0200

    Add build configuration for gristc executable

---
+++ build.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "gristc",
+        .root_source_file = b.path("bootstrap/bootstrap.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibC();
+    b.installArtifact(exe);
+}
